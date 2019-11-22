e85bbe729e841611a358a6d7af1fdec80069c9e9
slorello89 <slorello89@gmail.com>
2019-11-22 17:32:30 -0500

    first commit

---
+++ TranscriptionBlogPostDemo/Controllers/VoiceController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Nexmo.Api.Voice.Nccos;
+using Nexmo.Api.Voice.Nccos.Endpoints;
+
+namespace TranscriptionBlogPostDemo.Controllers
+{
+    public class VoiceController : Controller
+    {
+        const string BASE_URL = "BASE_URL";
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public HttpStatusCode Events()
+        {
+            return HttpStatusCode.OK;
+        }
+
+        [HttpGet]
+        public string Answer()
+        {
+            var webSocketAction = new ConnectAction()
+            {
+                Endpoint = new[]
+                {
+                    new WebsocketEndpoint()
+                    {
+                        Uri = $"wss://{BASE_URL}/ws",
+                        ContentType="audio/l16;rate=8000",                        
+
+                    }
+                }
+            };
+
+            var ncco = new Ncco(webSocketAction);
+            return ncco.ToString();
+        }
+    }
+}
---
+++ TranscriptionBlogPostDemo/TranscriptionEngine.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Diagnostics;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace TranscriptionBlogPostDemo
+{
+    public class TranscriptionEngine : IDisposable
+    {
+        const int SAMPLES_PER_SECOND = 8000;
+        const int BITS_PER_SAMPLE = 16;
+        const int NUMBER_OF_CHANNELS = 1;
+        const int BUFFER_SIZE = 160 * 2;
+
+        SpeechConfig _config = SpeechConfig.FromSubscription("Subscription_key", "region");
+        PushAudioInputStream _inputStream = AudioInputStream.CreatePushStream(AudioStreamFormat.GetWaveFormatPCM(SAMPLES_PER_SECOND, BITS_PER_SAMPLE, NUMBER_OF_CHANNELS));
+        AudioConfig _audioInput;
+        SpeechRecognizer _recognizer;
+        bool _started = false;
+
+        public TranscriptionEngine()
+        {
+            _audioInput = AudioConfig.FromStreamInput(_inputStream);
+        }
+
+        private void RecognizerRecognized(object sender, SpeechRecognitionEventArgs e)
+        {
+            Trace.WriteLine("Recognized: " + e.Result.Text);
+        }
+
+        private async Task StartSpeechTranscriptionEngine(string language)
+        {
+            _config.SpeechRecognitionLanguage = language;
+            _recognizer = new SpeechRecognizer(_config, _audioInput);
+            _recognizer.Recognized += RecognizerRecognized;
+            await _recognizer.StartContinuousRecognitionAsync();
+        }
+
+        private async Task StopTranscriptionEngine()
+        {
+            if(_recognizer != null)
+            {
+                _recognizer.Recognized -= RecognizerRecognized;
+                await _recognizer.StopContinuousRecognitionAsync();
+            }
+        }
+
+        public void Dispose()
+        {
+            _inputStream.Dispose();
+            _audioInput.Dispose();
+            _recognizer.Dispose();
+        }
+
+        public async Task ReceiveAudioOnWebSocket(HttpContext context, WebSocket webSocket)
+        {
+            var buffer = new byte[BUFFER_SIZE];
+
+            try
+            {
+                var language = "en-US";
+                await StartSpeechTranscriptionEngine(language);
+                WebSocketReceiveResult result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+                while (!result.CloseStatus.HasValue)
+                {
+                    await webSocket.SendAsync(new ArraySegment<byte>(buffer, 0, result.Count), result.MessageType, result.EndOfMessage, CancellationToken.None);
+
+                    result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+
+                    _inputStream.Write(buffer);
+                }
+                await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
+            }
+            catch (Exception e)
+            {
+                Trace.WriteLine(e.ToString());
+            }
+            finally
+            {
+                await StopTranscriptionEngine();
+            }
+        }    
+    }
+}
